032f82eb0de91f3b0a4e39e61d82a9900da7cd8c
Mehdi <mmouslih30@gmail.com>
2023-06-17 10:15:57 -0700

    task 3 lite

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: print low case alphabet
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
