641f4ee9c68dbe496aa192f8b217167435161317
Mehdi <mmouslih30@gmail.com>
2023-06-17 10:40:23 -0700

    task 44

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,18 @@ int main(void)
 {
 	char ch = 'a', ch1 = 'A';
 
-	while (ch <= 'z' && ch1 <= 'Z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
-		putchar(ch1);
 		ch++;
+	}
+	while (ch1 <= 'Z')
+	{
+		putchar(ch1);
 		ch1++;
 	}
 	putchar('\n');
 
 	return (0);
 }
+
