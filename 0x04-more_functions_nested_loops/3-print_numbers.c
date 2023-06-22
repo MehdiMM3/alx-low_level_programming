3f6c4c4e47d5a1213267d52e027fd648053a2faf
Mehdi <mmouslih30@gmail.com>
2023-06-22 12:33:00 -0700

    task 3 test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+		_putchar(n);
+	_putchar('\n');
+
+	return (0);
+}
