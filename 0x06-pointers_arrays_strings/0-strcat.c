d6c456a2db4da950ea5fb3978927921c5559f7e9
Mehdi <mmouslih30@gmail.com>
2023-06-29 11:26:51 -0700

    task 0 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,14 @@ char *_strcat(char *dest, char *src)
 {
 	char x = 0, y;
 
+	/*find the size of dest array*/
 	while (dest[x])
 		x++;
 
+	/*iterate through each src array  value without the null type*/
 	for (y = 0; src[y]; y++)
 	{
+		/*append src[y] to dest[x] while overwritting the null type in dest*/
 		dest[x] = src[y];
 		x++;
 	}
