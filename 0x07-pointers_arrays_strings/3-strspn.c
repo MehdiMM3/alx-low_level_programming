77027537b97826bb33ab747288d4be1be135c1b8
Mehdi <mmouslih30@gmail.com>
2023-07-03 07:15:25 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length
+ * @s: string
+ * @accept: bytes
+ * return: unsigned int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accpet[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
