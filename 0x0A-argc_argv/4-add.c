944c384d7b0158fc19e7c10f32b5ce8b87d417c1
Mehdi <mmouslih30@gmail.com>
2023-07-07 15:37:41 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start here
+ * @@argc: number
+ * @argv: pointer
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
