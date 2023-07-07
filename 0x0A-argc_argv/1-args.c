8a55b431c0c0ee5c0bd9d96e8c57524c89c706eb
Mehdi <mmouslih30@gmail.com>
2023-07-07 15:06:21 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * @argc: number
+ * @argv: pointer
+ * Return: 0-success, non-zero-fail
+*/
+
+int main(int argc, *argv[])
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
