7692d21103ac4cfcac4d7bce476b18a03ef7e17e
Mehdi <mmouslih30@gmail.com>
2023-07-07 15:13:07 -0700

    task1 3

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: 0-success, non-zero-fail
 */
 
-int main(int argc, *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
