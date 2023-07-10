0ffc0c690936023724517141d161d8b8d9f420dc
Mehdi <mmouslih30@gmail.com>
2023-07-10 04:56:04 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strdup - return a pointer
+ * @str: string
+ * Return: 0
+*/
+
+char *_strdup(char *str);
+{
+	int i = 0, s = 0;
+	char = *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[s] != '\0'; s++)
+		;
+
+	m = malloc(s * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < s; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
