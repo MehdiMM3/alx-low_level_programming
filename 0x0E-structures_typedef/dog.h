145cfbbad6593729f084744dbe64b218c94dac90
Mehdi <mmouslih30@gmail.com>
2023-07-17 08:44:01 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * description: ggg
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
