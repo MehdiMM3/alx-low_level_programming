521cb4a7513fa9c814dea8fa6155ef018f91555a
Mehdi <mmouslih30@gmail.com>
2023-07-17 15:38:24 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog
+ * @d: the dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
