d8ed082a0791f55159381c36dd03d49d51958e6e
Mehdi <mmouslih30@gmail.com>
2023-07-19 08:27:57 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: prints
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *));
+{
+	if (name && f)
+		f(name);
+}
