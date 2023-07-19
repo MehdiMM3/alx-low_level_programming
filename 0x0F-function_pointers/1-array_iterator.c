e80723e9d09c999b641bba7d3ec529d2ed0a2e9a
Mehdi <mmouslih30@gmail.com>
2023-07-19 08:37:03 -0700

    task1 cor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @action: action
  * Return: void
 */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int *end = array + size - 1;
 
