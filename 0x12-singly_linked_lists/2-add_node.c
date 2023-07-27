0da771165168b535f9aa4c79f09dea3201d02b92
Mehdi <mmouslih30@gmail.com>
2023-07-27 07:24:24 -0700

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - adds
+ * @head: address
+ * @str: str
+ * Return: size of list
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (!head || !new_head)
+		return (NULL);
+	if (str)
+	{
+		new_head->str = strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = _strlen(new_head->str);
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
