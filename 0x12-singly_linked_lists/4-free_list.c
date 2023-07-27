76bad89fc959a306d32afc04d63cda714d5eff80
Mehdi <mmouslih30@gmail.com>
2023-07-27 10:06:47 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - free
+ * @head: pointer
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
