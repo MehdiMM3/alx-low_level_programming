f16a0bd057ebf19a86f6aa6005497c9f843b850c
Mehdi <mmouslih30@gmail.com>
2023-07-27 10:01:50 -0700

    task3 cor

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,13 +9,13 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-	list_t*node = *head;
+	list_t *node = *head;
 
 	if (!head || !new_node)
 		return (NULL);
 	if (str)
 	{
-		new_mode->str = strdup(str);
+		new_node->str = strdup(str);
 		if (!new_node->str)
 		{
 			free(new_node);
