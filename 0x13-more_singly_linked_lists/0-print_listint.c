5813acbc167faa3660c96cb4dfa2ff2f4e5f91c2
Mehdi <mmouslih30@gmail.com>
2023-07-31 10:59:44 -0700

    task0 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print
+ * @h: pointer
+ * Return: size
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
