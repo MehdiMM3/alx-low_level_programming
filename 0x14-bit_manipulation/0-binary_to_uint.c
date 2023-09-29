f7269df8f9583bb4e7fa6cb344f47c2ae5038f58
mehdi <mmouslih30@gmail.com>
2023-09-29 10:36:10 -0700

    corr

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,13 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x = 0;
-	
+
 	if (b == NULL)
 		return (0);
-	
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		
 		x = (x << 1) | (*b - '0');
 		b++;
 	}
