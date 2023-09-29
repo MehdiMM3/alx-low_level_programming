d93789f72f9f03308acd0a8938ed3e829ce22104
mehdi <mmouslih30@gmail.com>
2023-09-29 11:17:31 -0700

    task0 corr

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint - Converts a binary number.
- * @b: The binary string.
- * Return: The conv number, or 0 if there are invalid chars or NULL.
+ * binary_to_uint - Converts.
+ * @b: string.
+ * Return: conv numb, or 0 if there are invalid chars or NULL.
 */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		x = (x << 1) | (*b - '0');
-		b++;
+		x = x * 2 + (*b++ - '0');
 	}
 	return (x);
 }
