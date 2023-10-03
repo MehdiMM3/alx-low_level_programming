86894462fa03983fccf7a3ee2a29ed2bda0596f6
mehdi <mmouslih30@gmail.com>
2023-10-03 09:34:01 -0700

    task1 corcor

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen - return
- * @s: strg
- * Return: int
-*/
+ * _strlen - return the length of a string
+ * @s: string
+ * Return: length of the string
+ */
 int _strlen(char *s)
 {
 	int x = 0;
@@ -18,24 +18,29 @@ int _strlen(char *s)
 
 /**
  * create_file - create a file
- * Description: description
- * @filename: name
- * @text_content: text
- * Retrun: 1 on success 0 on failure
-*/
-
+ * @filename: name of the file to create
+ * @text_content: text content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int x;
-	ssize_t b = 0, l = _strlen(text_content);
+	ssize_t b = 0, l;
 
 	if (!filename)
 		return (-1);
+
 	x = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (x == -1)
 		return (-1);
-	if (l)
-		b = write(x, text_content, l);
+
+	if (text_content)
+	{
+		l = _strlen(text_content);
+		if (l)
+			b = write(x, text_content, l);
+	}
+
 	close(x);
 	return (b == l ? 1 : -1);
 }
