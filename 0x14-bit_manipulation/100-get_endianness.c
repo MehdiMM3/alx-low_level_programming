0e35b7984f1ab58845e5b5d83deab8412306773e
mehdi <mmouslih30@gmail.com>
2023-10-26 09:31:18 -0700

    task100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - return it to the system
+ * Return: 0 if big, 1 if not
+*/
+int get_endianness(void)
+{
+	unsigned long int ez = 1;
+
+	return (*(char *)&n);
+}
