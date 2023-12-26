3363236da0fcdd13aba129ead7194c24f67409e5
mehdi <mmouslih30@gmail.com>
2023-12-25 23:16:35 -0800

    ekd

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,7 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *azq;
-	char hzw = 0;
+	char flag = 0; /* to check if we need comma */
 
 	if (ht == NULL || ht->array == NULL)
 		return;
@@ -19,10 +19,10 @@ void hash_table_print(const hash_table_t *ht)
 		azq = ht->array[i];
 		while (azq != NULL)
 		{
-			if (hzw == 1)
+			if (flag == 1)
 				printf(", ");
 			printf("'%s': '%s'", azq->key, azq->value);
-			hzw = 1;
+			flag = 1;
 			azq = azq->next;
 		}
 	}
